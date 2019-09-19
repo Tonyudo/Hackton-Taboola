1f7fe1110f5c27f0a13b1df2c800fcccb7e73ede
Tonyudo <Tonyyudo@gmail.com>
2019-09-19 21:04:28 +0300

    Add files via upload

---
+++ Hackton-Taboola-master/popup.js
@@ -0,0 +1,30 @@
+console.log("ONCLICK STARTED RUNNING");
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.extension.getBackgroundPage().console.log('foo');
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		var thisTab = chrome.tabs[0];
+		chrome.tabs.executeScript(thisTab.id, {file: 'variable-checker.js'}, function(data) {
+			console.log(response);
+			callback(response);
+			var response = data[0].response;
+		});
+	});
+
+});
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+	document.getElementById('bClick').addEventListener('click',onClick);
+
+});
+
+
+// function chooseIssue(){
+//
+
+
+function onClick(){
+	var newURL =  document.getElementById('issuetype').value;
+	console.log(newURL);
+	chrome.tabs.create({ url: newURL });
+}
